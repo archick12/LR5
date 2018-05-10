13cbb4009d04716c9ca1d5172a7c2deefadd77f9
archick12 <a.a.piluck@gmail.com>
2018-05-10 21:18:29 +0300

    fix

---
+++ src/XMLFileParser.java
@@ -0,0 +1,22 @@
+public class XMLFileParser implements MyCustomFileParserInterface{
+
+    @Override
+    public boolean findWord(String word) {
+        return false;
+    }
+
+    @Override
+    public boolean replaceWord(String word) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteWord(String word) {
+        return false;
+    }
+
+    @Override
+    public boolean addWord(String word) {
+        return false;
+    }
+}
