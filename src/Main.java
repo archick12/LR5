9a80a2918b4b145a1e6fdc605a56c639161f8b91
archick12 <a.a.piluck@gmail.com>
2018-05-10 21:28:15 +0300

    fix

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,17 @@
  * Created by User on 10.05.2018.
  */
 public class Main {
+    public static void main(String[] args) {
+        MyCustomFileParserInterface myP = null;
+        if (args[0].equals("JSON")) {
+            myP = new JSONFileParser();
 
+        } else if (args[0].equals("XML")) {
+            myP = new XMLFileParser();
+        }
 
+        myP.addWord("my word");
+        myP.replaceWord("my word to replace");
+
+    }
 }
