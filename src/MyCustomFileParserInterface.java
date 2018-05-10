56e6be2259af6a4222bc36ca48ba91ebb676b592
archick12 <a.a.piluck@gmail.com>
2018-05-10 20:53:54 +0300

    fix

--- src/MyCustomFileParserInterface.java
+++ src/MyCustomFileParserInterface.java
@@ -1,7 +1,5 @@
-/**
- * Created by User on 10.05.2018.
- */
-public interface MyCustomFileParser {
+
+public interface MyCustomFileParserInterface {
     public boolean findWord(String word);
     public boolean replaceWord(String word);
     public boolean deleteWord(String word);
